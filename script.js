74a96f611f12bc7b05b5d780d4fbd3df2db64e25
noahendale <noah.endale@gmail.com>
2016-09-05 18:49:30 -0400

    adding final changes

--- script.js
+++ script.js
@@ -1,83 +1,83 @@
 var characters = {
-	Lando: {
+	lando: {
 		armWrestle: 60,
 		foodEating: 80,
 		debate: 65,
 		footRace: 60
 	},
-	C3PO: {
+	c3PO: {
 		armWrestle: 40,
 		foodEating: 40,
 		debate: 55,
 		footRace: 40
 	},
-	Stormtrooper: {
+	stormtrooper: {
 		armWrestle: 50,
 		foodEating: 70,
 		debate: 35,
 		footRace: 65
 	},
-	Tuskenraider: {
+	tuskenraider: {
 		armWrestle: 70,
 		foodEating: 95,
 		debate: 40,
 		footRace: 75
 	},
-	Jawa: {
+	jawa: {
 		armWrestle: 45,
 		foodEating: 55,
 		debate: 45,
 		footRace: 50
 	},
-	Palpatine: {
+	palpatine: {
 		armWrestle: 95,
 		foodEating: 60,
 		debate: 80,
 		footRace: 90
 	},
-	Darthvader: {
+	darthvader: {
 		armWrestle: 85,
 		foodEating: 45,
 		debate: 85,
 		footRace: 80
 	},
-	Hansolo: {
+	hansolo: {
 		armWrestle: 65,
 		foodEating: 75,
 		debate: 60,
 		footRace: 85
 	},
-	Chewbacca: {
+	chewbacca: {
 		armWrestle: 75,
 		foodEating: 100,
 		debate: 50,
 		footRace: 45
 	},
-	Luke: {
+	luke: {
 		armWrestle: 90,
 		foodEating: 85,
 		debate: 70,
 		footRace: 70
 	},
-	Obiwan: {
+	obiwan: {
 		armWrestle: 80,
 		foodEating: 65,
 		debate: 90,
 		footRace: 55
 	},
-	Leia: {
+	leia: {
 		armWrestle: 55,
 		foodEating: 90,
 		debate: 75,
 		footRace: 100
 	},
-	R2D2: {
+	r2D2: {
 		armWrestle: 35,
 		foodEating: 35,
 		debate: 95,
 		footRace: 35
 	},
-	Yoda: {
+	yoda: {
 		armWrestle: 100,
 		foodEating: 50,
 		debate: 100,
@@ -114,19 +114,18 @@ $('form').on('submit', function(event) {
 	//get corresponding stat based on battle type
 
 	//use Object.assign to make a copy.
-	//This is an ES6 so browser support is not 
 	var stats1 = Object.assign({},characters[userCharSelect1]);
 	var stats2 = Object.assign({},characters[userCharSelect2]);
 
 	var x = Math.floor(Math.random()) * 100;
 	var bonus = false;
 	if (stats1[userSelectedBattle] > stats2[userSelectedBattle]) {
-		if((Math.floor((Math.random()) * 100)) >= 50) {
+		if((Math.floor((Math.random()) * 100)) >= 90) {
 			stats2[userSelectedBattle] = stats2[userSelectedBattle] + 30;
 			bonus = true;
 		}
 	} else {
-		if((Math.floor((Math.random()) * 100)) >= 50) {
+		if((Math.floor((Math.random()) * 100)) >= 90) {
 			stats1[userSelectedBattle] = stats1[userSelectedBattle] + 30;
 			bonus = true;
 		}
@@ -148,6 +147,7 @@ $('form').on('submit', function(event) {
 
 var displayWinner = function(winner, bonus){
 	$('.winner').html(`<h2 class = "winnerName">${winner} is victorious!</h2>`);
+	$('.winnerName').css('text-transform', 'capitalize');
 	$('.winner').css('height', '60vh');
 	$('.winner').css('margin-top', '10vh');
 	$('.winner').append(`<img src="sprites/${winner}.png">`);
